b86271bf493957824488af49f5f306282e32d611
Mateusz <mateusz@pop-os.localdomain>
2024-01-19 22:03:59 +0100

    populacja z mst

--- am/src/main/java/org/example/lista4/Lista4.java
+++ am/src/main/java/org/example/lista4/Lista4.java
@@ -7,14 +7,24 @@ import java.io.IOException;
 
 public class Lista4 {
     public static void run() throws IOException, InterruptedException {
-        String filename = "bcl380.tsp";
+        String filename = "xqf131.tsp";
         FileHandler fh = new FileHandler("data/" + filename);
         Graph g = fh.makeGraph();
         int[][] distMatrix = g.calcDistMatrix();
         int V = g.getNodes().size();
+        int ov = 0;
+        int min = Integer.MAX_VALUE;
+        int r = 20;
+        for(int i=0; i<r; i++){
+            int w = PopulationSimulation.run(distMatrix,V);
+            if(w<min){
+                min = w;
+            }
+            ov += w;
+            System.out.println("iteracja " + i);
+        }
 
-
-        int w = PopulationSimulation.run(distMatrix,V);
-        System.out.println(w);
+        System.out.println(ov/r);
+        System.out.println(min);
     }
 }
--- am/src/main/java/org/example/lista4/PopulationSimulation.java
+++ am/src/main/java/org/example/lista4/PopulationSimulation.java
@@ -1,6 +1,7 @@
 package org.example.lista4;
 
 import org.example.Conv;
+import org.example.MST;
 
 import java.util.*;
 
@@ -10,40 +11,40 @@ public class PopulationSimulation{
 
         List<Individual> selected = new ArrayList<>(population);
         Collections.shuffle(selected);
-        selected = selected.subList(0, crossovers);
+        selected = selected.subList(0,crossovers);
+        Collections.sort(selected);
 
-        while (parents.size() != 2) {
-            Individual parent = selected.stream()
-                    .min(Comparator.comparingInt(Individual::getWeight))
-                    .orElse(null);
-            parents.add(parent);
-            selected.remove(parent);
-        }
+        parents.add(selected.get(0));
+        parents.add(selected.get(1));
 
         return parents;
     }
     public static int run(int[][] distMatrix, int V){
+        Random random = new Random();
         double mutation_chance = 0.1;
         double crossover_chance = 0.9;
-        int crossovers = V/5;
+        int crossovers = V /5;
 
         List<Individual> population = new ArrayList<>();
-        for(int i=0; i<V; i++){
-            List<Integer> conv = Conv.getRandomConv(V);
+        MST mst = new MST(V);
+        int[] parent = mst.primMST(distMatrix);
+        for(int i = 0; i< V; i++){
+            Conv c = new Conv(parent,i);
+            List<Integer> conv = c.getConv();
             int w = Conv.calcWeight(distMatrix,conv);
             population.add(new Individual(conv,w));
         }
         Collections.sort(population);
         int best_w = population.get(0).getWeight();
 
-        Random random = new Random();
+
         int without_imp = 0;
         while(without_imp<V){
             //Krzyzowanie
             List<Individual> children_all = new ArrayList<>();
             for(int i=0; i<crossovers; i++){
                 if(random.nextDouble() < crossover_chance){
-                    List<Individual> parents = tournament(population,crossovers);
+                    List<Individual> parents = tournament(population,crossovers/2);
                     List<Integer> p1 = parents.get(0).getConv();
                     List<Integer> p2 = parents.get(1).getConv();
                     List<List<Integer>> children = PMXCrossover.crossover(p1,p2);
@@ -63,7 +64,7 @@ public class PopulationSimulation{
 
             //Selekcja
             Collections.sort(population);
-            population = new ArrayList<>(population.subList(0,V));
+            population = new ArrayList<>(population.subList(0, V));
             int curr_best = population.get(0).getWeight();
             if(curr_best >= best_w){
                 without_imp += 1;
