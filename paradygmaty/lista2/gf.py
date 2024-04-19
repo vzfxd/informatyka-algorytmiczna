0966f13b8e96e38bda6358bff0aa26599f2ca808
mateusz <mateusznasewicz@gmail.com>
2024-04-19 10:54:36 +0200

    update

---
+++ paradygmaty/lista2/gf.py
@@ -0,0 +1,63 @@
+class GF:
+    def __init__(self, val: int, characteristic = 1234577 ):
+        self.characteristic = characteristic
+        self.val = val % characteristic
+
+    def __str__(self):
+        return str(f"{self.val}, {self.characteristic}")
+    
+    def __lt__(self, other):
+        GF.__check_err(self, other)
+        return self.val < other.val
+
+    def __gt__(self, other):
+        GF.__check_err(self, other)
+        return self.val > other.val
+
+    def __le__(self, other):
+        GF.__check_err(self, other)
+        return self.val <= other.val
+
+    def __ge__(self, other):
+        GF.__check_err(self, other)
+        return self.val >= other.val
+
+    def __eq__(self, other):
+        GF.__check_err(self, other)
+        return self.val == other.val
+
+    def __ne__(self, other):
+        GF.__check_err(self, other)
+        return self.val != other.val
+
+    def __add__(self, other):
+        GF.__check_err(self, other)
+        return GF( (self.val + other.val ) % self.characteristic, self.characteristic )
+
+    def __sub__(self, other):
+        GF.__check_err(self, other)
+        return GF( (self.val + (-other.val % other.characteristic) ) % self.characteristic, self.characteristic )
+
+    def __mul__(self, other):
+        GF.__check_err(self, other)
+        return GF( (self.val * other.val ) % self.characteristic, self.characteristic )
+
+    def __truediv__(self, other):
+        GF.__check_err(self, other)
+        gcd, x, _ = GF.__euklides(other.val, other.characteristic)
+    
+        if(gcd != 1):
+            return -1
+        
+        return GF( (self.val * (x % other.characteristic)) % self.characteristic, self.characteristic )
+
+    def __euklides(a,b):
+        if b == 0:
+            return a, 1, 0
+        else:
+            d, x, y = GF.__euklides(b, a % b)
+        return d, y, x - (a // b) * y
+    
+    def __check_err(c1, c2):
+        if(c1.characteristic != c2.characteristic):
+            raise ValueError
---
+++ paradygmaty/lista2/test.py
@@ -0,0 +1,57 @@
+from gf import GF
+
+# Addition
+a = GF(3)
+b = GF(7)
+c = GF(10)
+
+print()
+print(f"a = GF(3)\nb = GF(7)\nc = GF(10)")
+print(f"a + b = {a+b}")
+a += b
+print(f"a += b || a = {a}")
+print(f"a == c {a == c}")
+
+# Substraction
+a = GF(915)
+b = GF(916)
+c = GF(1234576)
+
+print()
+print(f"a = GF(915)\nb = GF(916)\nc = GF(1234576)")
+print(f"a - b = {a-b}")
+print(f"a - b == c {a-b==c}")
+a -= b
+print(f"a -= b || a = {a}")
+print(f"a == c {a == c}")
+
+# Multiplication
+a = GF(12)
+b = GF(88)
+c = GF(1056)
+
+print()
+print(f"a = GF(12)\nb = GF(88)\nc = GF(1056)")
+print(f"a * b = {a*b}")
+print(f"a * b == c {a*b==c}")
+a *= b
+print(f"a *= b || a = {a}")
+print(f"a == c {a == c}")
+print(f"a > b {a > b}")
+print(f"a >= b {a >= b}")
+print(f"b < a {a < b}")
+print(f"b <= a {a <= b}")
+
+
+# Division
+a = GF(88)
+b = GF(11)
+c = GF(8)
+
+print()
+print(f"a = GF(88)\nb = GF(11)\nc = GF(8)")
+print(f"a / b = {a / b}")
+print(f"a / b == c {a / b == c}")
+print(f"a /= b")
+a /= b
+print(f"a == c {a == c}")
